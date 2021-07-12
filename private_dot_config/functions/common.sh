8472d66e0c2fa0b80a1353d409b60e02f407c32d
Alex Lov <alex@alexlov.com>
2021-07-12 11:22:24 +0300

    [profile] Move getting shell name to functions

--- private_dot_config/functions/common.sh
+++ private_dot_config/functions/common.sh
@@ -1,3 +1,11 @@
 function has() {
   which $1 >/dev/null
 }
+
+function get_shell_name() {
+  if [ -n "${ZSH_VERSION}"]; then
+    echo -n "zsh"
+  elif [ -n "${BASH_VERSION}" ]; then
+    echo -n "bash"
+  fi
+}
