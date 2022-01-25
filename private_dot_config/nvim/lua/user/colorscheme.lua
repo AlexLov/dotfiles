0fed5e44801a691eaca9cc33d11b5521123ae56d
Alex Lov <alex@alexlov.com>
2022-01-25 21:07:06 +0300

    [nvim] Use vim.g to set colorscheme parameters

--- private_dot_config/nvim/lua/user/colorscheme.lua
+++ private_dot_config/nvim/lua/user/colorscheme.lua
@@ -1,15 +1,16 @@
+-- the configuration options should be placed before `colorscheme sonokai`.
+-- available values: `'default'`, `'atlantis'`, `'andromeda'`, `'shusia'`, `'maia'`, `'espresso'`
+vim.g.sonokai_style = 'default'
+vim.g.sonokai_enable_italic = 1
+vim.g.sonokai_diagnostic_text_highlight = 1
+vim.g.sonokai_diagnostic_line_highlight = 1
+vim.g.sonokai_transparent_background = 1
+vim.g.sonokai_menu_selection_background = 'green'
+vim.g.sonokai_better_performance = 1
+vim.g.sonokai_spell_foreground = 'colored'
+
 vim.cmd [[
 try
-	" The configuration options should be placed before `colorscheme sonokai`.
-	" Available values: `'default'`, `'atlantis'`, `'andromeda'`, `'shusia'`, `'maia'`, `'espresso'`
-	let g:sonokai_style = 'default'
-	let g:sonokai_enable_italic = 1
-	let g:sonokai_diagnostic_text_highlight = 1
-	let g:sonokai_diagnostic_line_highlight = 1
-	let g:sonokai_transparent_background = 1
-	let g:sonokai_menu_selection_background = 'green'
-	let g:sonokai_better_performance = 1
-
 	colorscheme sonokai
 catch /^Vim\%((\a\+)\)\=:E185/
 	colorscheme default
