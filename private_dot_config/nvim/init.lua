1abb78e8229a95c751514003d4c33ad933d30bd0
Alex Lov <alex@alexlov.com>
2022-03-28 00:56:05 +0300

    [nvim] Replace lua config with nyoom

--- private_dot_config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
--- Based on https://github.com/LunarVim/Neovim-from-scratch
-require "user.options"
-require "user.plugins"
-require "user.autocommands"
-require "user.keymaps"
-require "user.colorscheme"
-require "user.whitespace"
-require "user.indent-blankline"
-require "user.nvim-tree"
