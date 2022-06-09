370efab4fed0bf34b737ffc461c36b0c98fc5567
Alex Lov <alex@alexlov.com>
2022-06-09 13:28:59 +0300

    [gcloud] Enable new gcloud auth plugin

--- private_dot_config/zsh/conf.d/99_google_cloud_sdk.zsh
+++ private_dot_config/zsh/conf.d/99_google_cloud_sdk.zsh
@@ -4,3 +4,6 @@ if [ -f '/home/alex/.local/share/google-cloud-sdk/path.zsh.inc' ]; then . '/home
 
 # The next line enables shell command completion for gcloud.
 if [ -f '/home/alex/.local/share/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/alex/.local/share/google-cloud-sdk/completion.zsh.inc'; fi
+
+# https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
