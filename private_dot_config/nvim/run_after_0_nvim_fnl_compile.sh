79d6876c0f63e8b9d3bc1aa94aab6ce818a97930
Alex Lov <alex@alexlov.com>
2022-08-23 22:41:56 +0300

    [nvim] Fix scrpits to update packer and treesitter

--- private_dot_config/nvim/run_after_0_nvim_fnl_compile.sh
+++ private_dot_config/nvim/run_after_0_nvim_fnl_compile.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-echo 'Run FnlCompile'
 nvim --headless +FnlCompile +q
--- private_dot_config/nvim/run_once_after_2_nvim_ts_update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# fnl/pack/treesitter.fnl hash: {{ include "fnl/pack/treesitter.fnl" | sha256sum }}
-echo 'Run TSUpdate'
-nvim --headless +TSUpdate	+q
