ce9e63f21fe5da8a3aa512efc942ef390148f4a0
Alex Lov <alex@alexlov.com>
2022-08-23 22:45:59 +0300

    [bat] Fix script to automatically update cache with themes/syntaxes

--- private_dot_config/bat/run_once_after_bat_cache_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# syntaxes/Fennel.sublime-syntax hash: {{ include "Fennel.sublime-syntax" | sha256sum }}
-# syntaxes/Bats.sublime-syntax hash: {{ include "Bats.sublime-syntax" | sha256sum }}
-# themes/Everblush.tmTheme hash: {{ include "themes/Everblush.tmTheme" | sha256sum }}
-bat cache --build
