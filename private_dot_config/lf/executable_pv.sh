2e50b7d70a4f97725bc0f42926178480e0808f4a
Alex Lov <alex@alexlov.com>
2022-12-11 16:29:41 +0700

    [lf] Use chafa as previewer of images with sixel

--- private_dot_config/lf/executable_pv.sh
+++ private_dot_config/lf/executable_pv.sh
@@ -7,6 +7,8 @@ case "$1" in
   *.7z) 7z l "$1";;
   *.pdf) pdftotext "$1" -;;
   *.md) glow -s dark "$1";;
+  *.bmp|*.jpg|*.jpeg|*.png|*.xpm|*.webp|*.gif|*.jfif)
+    chafa "${1}" -f sixel -s "$((${2}-2))x${3}" | sed 's/#/\n#/g';;
   *) bat --color=always --paging=never "$1";;
 esac
 
